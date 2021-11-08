2834ddb86b1563a837e24892a8ddbea1b2415887
Dario <dario.defrancisci92@gmail.com>
2021-11-08 20:40:11 +0100

    upgrade show.blade.php

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -79,7 +79,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        dd($post);
+        //dd($post);
         if (!$post) {
             abort(404);
         }
