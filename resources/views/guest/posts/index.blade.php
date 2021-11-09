dc310f4b60ced70b595bbf33fc4f6b09bb3c24ab
Dario <dario.defrancisci92@gmail.com>
2021-11-09 13:45:48 +0100

    Creazione tabella tags

--- resources/views/guest/posts/index.blade.php
+++ resources/views/guest/posts/index.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app');
 
 @section('content')
-   <ul>
+    <h1 class="m-5">Post visualizzati da utenti non registrati</h1>
+   <ul class="m-5">
         @foreach ($posts as $post)
             <li><a href="{{ route('posts.show', $post->slug)}}">{{ $post->title}}</a></li>
         @endforeach
